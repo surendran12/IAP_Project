f9fb0f0ffa4f431b983a39608ae48e931638ce8c
Surendran Natarajan <surendran.sns@gmail.com>
2024-03-05 14:52:21 +0530

    theme
    theme

--- App.js
+++ App.js
@@ -1,53 +1,178 @@
-import logo from './logo.svg';
-import './App.css';
-import ApicallFetch from './components/apicall';
-import UseState from './components/usestate';
-import UseEffect from './components/useeffect';
-import UseContext from './components/usecontext';
-import UseCallback from './components/usecallback';
-import UseReducer from './components/usereducer';
-import UseRef from './components/useref';
-import UseMemo from './components/usememo';
-import ReactQuestion from './components/reactquestion';
-import Norender from './components/Norender';
-import Nprevstate from './components/Nprevstate';
-import JavascriptQA from './components/javascriptQA';
-import ReactQA from './components/reactQA ';
-import Function from './components/function';
-import ApiRedux from './components/ApiRedux';
-import HtmlCss from './components/htmlcss';
-import AngularUi from './components/angular';
-import Frontend from './components/Frontend';
-import Test from './components/test';
-import ApiProject from './components/apiproject';
 
+
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <ApiProject /> */}
-      <Test />
-     {/* <Frontend /> */}
-      {/* <AngularUi /> */}
-     {/* <JavascriptQA />
-       <ReactQA />
-      <ApiRedux />
-      <HtmlCss /> */}
-       {/* <Function /> */}
-        {/* <ApicallFetch /> */}
-       {/* <UseState />
-         <UseEffect />
-        <UseContext />
-        <UseCallback />
-        <UseReducer />
-        <UseMemo/> */}
-        {/*useRef Example*/}
-          {/* <UseRef /> */}
-          {/* <Norender />
-          <Nprevstate /> */}
-        {/*useRef Example*/}
+  const initialBoard = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ];
+  const [board, setBoard] = useState(initialBoard);
+  const [direction, setDirection] = useState(null);
+  const [placement, setPlacement] = useState({ x: -1, y: -1, arrow: null });
 
-       {/* <ReactQuestion />*/}
+  const setBoardData = (x, y) => {
+    if (x === null || y === null || !placement.arrow) {
+      return alert(`Select the ${!x ? "x value" : !y ? "y value" : "Bot facing direction"}`);
+    }
+    console.log(placement);
+    if (x >= 0 && x < initialBoard.length && y >= 0 && y < initialBoard[0].length) {
+      setBoard((brd) => {
+        brd = initialBoard;
+        brd[x][y] = 1;
+        return brd;
+      });
+      setPlacement((p) => ({ ...p, x: x, y: y }));
+    } else {
+      alert("Invalid Move");
+    }
+  };
+
+  const move = () => {
+    switch (direction) {
+      case "EAST": {
+        setBoardData(placement.x, placement.y + 1);
+
+        break;
+      }
+      case "WEST": {
+        setBoardData(placement.x, placement.y - 1);
+        break;
+      }
+      case "NORTH": {
+        setBoardData(placement.x - 1, placement.y);
+        break;
+      }
+      case "SOUTH": {
+        setBoardData(placement.x + 1, placement.y);
+        break;
+      }
+    }
+  };
+
+  const rotate = (left) => {
+    switch (placement.arrow) {
+      case "EAST": {
+        setPlacement((p) => ({ ...p, arrow: left ? "NORTH" : "SOUTH" }));
+        setDirection(left ? "NORTH" : "SOUTH");
+        break;
+      }
+      case "WEST": {
+        setPlacement((p) => ({ ...p, arrow: left ? "SOUTH" : "NORTH" }));
+        setDirection(left ? "SOUTH" : "NORTH");
+        break;
+      }
+      case "NORTH": {
+        setPlacement((p) => ({ ...p, arrow: left ? "WEST" : "EAST" }));
+        setDirection(left ? "WEST" : "EAST");
+        break;
+      }
+      case "SOUTH": {
+        setPlacement((p) => ({ ...p, arrow: left ? "EAST" : "WEST" }));
+        setDirection(left ? "EAST" : "WEST");
+        break;
+      }
+    }
+  };
+
+  return (
+    <div className="container">
+      <div>
+        {board.map((element, i) => {
+          return (
+            <div className="panel">
+              {element.map((data, j) => {
+                return (
+                  <p
+                    className={`${
+                      board[i][j]
+                        ? direction === "NORTH"
+                          ? "robotUp"
+                          : direction === "SOUTH"
+                          ? "robotDown"
+                          : direction === "EAST"
+                          ? "robotRight"
+                          : "robotLeft"
+                        : "block"
+                    }`}
+                  >
+                    {/* {i} {j} */}
+                  </p>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <div className="controls">
+        <div className="controlsview">
+          <select name="x" id="x" onChange={(e) => setPlacement({ ...placement, x: parseInt(e.target.value) })}>
+            <option disabled selected value={-1}>
+              x
+            </option>
+            {board.map((_, idx) => (
+              <option value={idx}>{idx}</option>
+            ))}
+          </select>
+          <select name="y" id="y" onChange={(e) => setPlacement({ ...placement, y: parseInt(e.target.value) })}>
+            <option disabled selected value={-1}>
+              y
+            </option>
+            {board.map((_, idx) => (
+              <option value={idx}>{idx}</option>
+            ))}
+          </select>
+          <select
+            name="direction"
+            id="direction"
+            onChange={(e) => setPlacement({ ...placement, arrow: e.target.value })}
+          >
+            <option disabled selected value={null}>
+              HEAD
+            </option>
+            <option value="EAST">EAST</option>
+            <option value="WEST">WEST</option>
+            <option value="NORTH">NORTH</option>
+            <option value="SOUTH">SOUTH</option>
+          </select>
+          <div>
+            <button className="placebtn"
+              onClick={(e) => {
+                setDirection(placement?.arrow ?? null);
+                setBoardData(placement.x, placement.y);
+              }}
+            >
+              Place
+            </button>
+          </div>
+        </div>
+        <br />
+        <div>
+          <button onClick={move}>Move</button>
+        </div>
+        <br />
+        <div>
+          <button onClick={() => rotate(true)}>Left</button>
+        </div>
+        <br />
+        <div>
+          <button onClick={() => rotate(false)}>Right</button>
+        </div>
+        <br />
+        <div>
+          <button
+            onClick={() => {
+              alert(`x: ${placement.x} y: ${placement.y} face: ${placement.arrow}`);
+            }}
+          >
+            Report
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
