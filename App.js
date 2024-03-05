7647886f12e6f653168a7a7a3e10fd15fc8b1e6a
Surendran Natarajan <surendran.sns@gmail.com>
2024-03-05 14:48:22 +0530

    theme
    theme color and css

---
+++ App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import ApicallFetch from './components/apicall';
+import UseState from './components/usestate';
+import UseEffect from './components/useeffect';
+import UseContext from './components/usecontext';
+import UseCallback from './components/usecallback';
+import UseReducer from './components/usereducer';
+import UseRef from './components/useref';
+import UseMemo from './components/usememo';
+import ReactQuestion from './components/reactquestion';
+import Norender from './components/Norender';
+import Nprevstate from './components/Nprevstate';
+import JavascriptQA from './components/javascriptQA';
+import ReactQA from './components/reactQA ';
+import Function from './components/function';
+import ApiRedux from './components/ApiRedux';
+import HtmlCss from './components/htmlcss';
+import AngularUi from './components/angular';
+import Frontend from './components/Frontend';
+import Test from './components/test';
+import ApiProject from './components/apiproject';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ApiProject /> */}
+      <Test />
+     {/* <Frontend /> */}
+      {/* <AngularUi /> */}
+     {/* <JavascriptQA />
+       <ReactQA />
+      <ApiRedux />
+      <HtmlCss /> */}
+       {/* <Function /> */}
+        {/* <ApicallFetch /> */}
+       {/* <UseState />
+         <UseEffect />
+        <UseContext />
+        <UseCallback />
+        <UseReducer />
+        <UseMemo/> */}
+        {/*useRef Example*/}
+          {/* <UseRef /> */}
+          {/* <Norender />
+          <Nprevstate /> */}
+        {/*useRef Example*/}
+
+       {/* <ReactQuestion />*/}
+    </div>
+  );
+}
+
+export default App;
